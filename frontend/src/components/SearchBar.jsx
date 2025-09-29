151153b80b85eb70f475094327eda46db4c15c10
Benjamin Inglis <bjammin134@gmail.com>
2025-09-29 15:54:07 -0700

    Anchor menu dropdown to input bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,14 @@
-import { Input, Center } from "@chakra-ui/react";
+"use client";
+
+import { Input, Center, Box, Button, Menu, Portal } from "@chakra-ui/react";
+import { useRef } from "react";
+
 import { useState } from "react";
 import { fetchSearchResults } from "../utils/js/apiCalls";
 import { debounce } from "lodash";
+
 import SearchResultsPreview from "./SearchResultsPreview";
+import SearchResultsPreviewCard from "./SearchResultsPreviewCard";
 
 export default function SearchBar() {
   const [searchResults, setSearchResults] = useState();
@@ -10,9 +16,10 @@ export default function SearchBar() {
   const debounceOnChange = debounce(async (e) => {
     const searchRes = await fetchSearchResults(e.target.value);
     setSearchResults(searchRes);
-    //TODO: finish implementing once we have a search card
   }, 500);
 
+  const ref = useRef();
+  const getAnchorRect = () => ref.current.getBoundingClientRect();
   //console.log(searchResults);
   return (
     <>
@@ -22,8 +29,35 @@ export default function SearchBar() {
           marginTop={12}
           w="calc(50vw)"
           onChange={debounceOnChange}
+          ref={ref}
         />
         <SearchResultsPreview searchResults={searchResults} />
+        <Menu.Root positioning={{ getAnchorRect }}>
+          <Menu.Trigger asChild>
+            <Button variant="outline" size="sm">
+              Open
+            </Button>
+          </Menu.Trigger>
+          {/* // TODO: Populate the menu items with the preview cards */}
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Menu.Item value="new-txt">
+                  <SearchResultsPreviewCard
+                    title="demo movie"
+                    img={`https://image.tmdb.org/t/p/w500/48KYBYnHmt7ykX57zGuv6SXVZOO.jpg`}
+                    year={2024}
+                    actors={["Bookie", "Cannoli"]}
+                  />
+                </Menu.Item>
+                <Menu.Item value="new-file">New File...</Menu.Item>
+                <Menu.Item value="new-win">New Window</Menu.Item>
+                <Menu.Item value="open-file">Open File...</Menu.Item>
+                <Menu.Item value="export">Export</Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
       </Center>
     </>
   );
