11eab607823a9540f5769bb0340a7ef930b14b90
Benjamin Inglis <bjammin134@gmail.com>
2025-10-17 09:51:44 -0700

    Configure vite options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import netlify from "@netlify@vite-plugin";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlify()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // 👈 enables "@/..."
